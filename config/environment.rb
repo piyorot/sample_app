9959f520b024377a707cb63719da66abd59f38d4
Piyorot <piyorot21@hotmail.com>
2012-06-03 11:28:11 +0700

    Finish user signup

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SampleApp::Application.initialize!
+
+# Speed up tests by lowering BCrypt's cost function.
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-
+  resources :users
+  root to: 'static_pages#home'
   get "static_pages/home"
-
   get "static_pages/help"
-
   get "static_pages/about"
-
   get "static_pages/contact"
+
+  match '/signup',  to: 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.

ac966f3fc8127e4a504e98909bad65ca1234f987
Piyorot <piyorot21@hotmail.com>
2012-06-09 16:54:47 +0700

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@
 
 class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password, :password_confirmation
+	has_many :microposts, dependent: :destroy
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	has_secure_password
 	
@@ -23,6 +24,11 @@ class User < ActiveRecord::Base
 	validates :password, presence: true, length: { minimum: 6 }
 	validates :password_confirmation, presence: true
 
+	def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  	end
+  	
 	private
 
     def create_remember_token
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,6 +15,22 @@ describe "StaticPages" do
       visit root_path
       page.should have_selector('title', :text => "#{base_title} | Home")
     end
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
   end
 
   describe "Help page" do
